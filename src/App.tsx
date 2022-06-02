9ecf7a74bf584822a1abd9383cbdb7f1663491ce
Chrisp <chris.pyke@messagebird.com>
2022-06-02 17:56:12 +0200

    build(app): some of the code that we worked through in the session

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
+import Grid from '@mui/material/Grid';
+import Textfield from '@mui/material/TextField';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
-import logo from './logo.svg';
+
+
+
+
 
 function App() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }
+
+  console.log(errors);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <input {...register("dummy", { required: true, maxLength: 80 })} />
+          </Grid>
+          <Grid item xs={12}>
+            <Textfield label="Outlined" variant="outlined"  {...register("dummy2", {
+              required: true,
+              minLength: {
+                value: 10,
+                message: "greater than 10"
+              },
+              maxLength: {
+                value: 15,
+                message: "less than 15"
+              },
+            })} error={!!errors.dummy2} helperText={errors.dummy2 && errors.dummy2.message} />
+          </Grid>
+          <Grid item xs={12}>
+            <button type="submit">Submit</button>
+          </Grid>
+        </Grid>
+      </form>
     </div>
   );
 }
